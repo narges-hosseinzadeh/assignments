4476fde921ea1af19fcd75bd426115afd8d7e1c2
narges hosseinzadeh <110340020+narges-hosseinzadeh@users.noreply.github.com>
2022-08-20 23:43:04 +0430

    Delete question6.py

--- question6.py
+++
@@ -1,5 +0,0 @@
-a=int(input('addadi delkhah vared konid= '))
-raghamedovom=((a//10)%10)
-raghamepanjom=((a//10000)%10)
-majmu=raghamedovom+raghamepanjom
-print('majmoe raghame dovom va panjom =' , majmu)
