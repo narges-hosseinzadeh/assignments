79735451de25049b6c06c276560ef27faeba8287
narges hosseinzadeh <110340020+narges-hosseinzadeh@users.noreply.github.com>
2022-08-20 23:42:09 +0430

    Add files via upload

---
+++ assignments1/question5.py
@@ -0,0 +1,10 @@
+r=float(input('shoae dayere ra vared konid :'))
+h=float(input('ertefae ostovane ra vared konid : '))
+mohitedayere=2*3.14*r
+masahatedayere=3.14*r*r
+masahatejanebi=mohitedayere*h
+hajm=masahatedayere*h
+masahatekol=2*masahatedayere+masahatejanebi
+print('masahate janebi =', masahatejanebi)
+print('masahate kol =', masahatekol)
+print('hajm = ', hajm)
